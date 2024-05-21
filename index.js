6044ba34fc1cac993d067284291faa0450dfbdba
rds <rasul@futureblur.com>
2024-05-21 19:13:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+require('dotenv').config();
+
+const config = require("./config.json");
+
+client.once('ready', () => {
+    console.log('ZIP is ready. 👁️');
+});
+
+// List of greeting messages
+const greetings = [
+    "yoo, what's poppin",
+    "happy to have you here!",
+    "welcome to the club 🎊",
+    "you made it, welcome!",
+    "a warm welcome from everyone.",
+    "hey hey, feel free to introduce yourself!",
+    "yoo",
+];
+
+// Function to get a random greeting message
+function getRandomGreeting() {
+    const randomIndex = Math.floor(Math.random() * greetings.length);
+    return greetings[randomIndex];
+}
+
+// Listen for new members joining the server
+client.on('guildMemberAdd', member => {
+    // Get a random greeting message
+    const greeting = getRandomGreeting();
+
+    // Send the greeting message to a specific channel
+    // Replace 'channel-id-here' with the ID of the channel where you want to send the message
+    const channel = member.guild.channels.cache.get(config.chatChannelId);
+    if (channel) {
+        channel.send(`${ member.user } ${ greeting }`);
+    }
+
+    // Automatically assign a role to the new member
+    // Replace 'role-id-here' with the ID of the role you want to assign
+    const role = member.guild.roles.cache.get(config.memberRoleId);
+    if (role) {
+        member.roles.add(role).catch(console.error);
+    }
+});
+
+// Ping command
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.content === '!ping') {
+        message.reply('Pong. 🏓');
+    }
+});
+
+// Login to Discord with your app's token from env file
+client.login(process.env.BOT_TOKEN);
