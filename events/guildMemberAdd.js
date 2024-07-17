fd50bed85b607f99b052d768d36d702f008b2f47
rds <rasul@futureblur.com>
2024-07-17 09:18:01 +0200

    feat: delay welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,44 +1,46 @@
 module.exports = {
-	name: 'guildMemberAdd',
-	execute(member, config) {
-		// List of greeting messages
-		const greetings = [
-			"yoo, what's poppin",
-			"happy to have you here!",
-			"welcome to the club 🎊",
-			"you made it, welcome!",
-			"a warm welcome from everyone.",
-			"hey hey, feel free to introduce yourself!",
-			"yoo",
-		];
+  name: "guildMemberAdd",
+  execute(member, config) {
+    // List of greeting messages
+    const greetings = [
+      "yoo, what's poppin",
+      "happy to have you here!",
+      "welcome to the club 🎊",
+      "you made it, welcome!",
+      "a warm welcome from everyone.",
+      "hey hey, feel free to introduce yourself!",
+      "yoo",
+    ];
 
-		// Function to get a random greeting message
-		function getRandomGreeting() {
-			const randomIndex = Math.floor(Math.random() * greetings.length);
-			return greetings[randomIndex];
-		}
+    // Function to get a random greeting message
+    function getRandomGreeting() {
+      const randomIndex = Math.floor(Math.random() * greetings.length);
+      return greetings[randomIndex];
+    }
 
-		// Get a random greeting message
-		const greeting = getRandomGreeting();
+    // Get a random greeting message
+    const greeting = getRandomGreeting();
 
-		// Send the greeting message to a specific channel
-		const channel = member.guild.channels.cache.get(config.channels.chat.id);
-		if (channel) {
-			channel.send(`${ member.user } ${ greeting }`);
-		}
+    // Send the greeting message to a specific channel
+    const channel = member.guild.channels.cache.get(config.channels.chat.id);
+    if (channel) {
+      setTimeout(() => {
+        channel.send(`${member.user} ${greeting}`);
+      }, 18000);
+    }
 
-		// Automatically assign a role to the new member
-		const role = member.guild.roles.cache.get(config.roles.member);
-		if (role) {
-			member.roles.add(role).catch(console.error);
-		}
+    // Automatically assign a role to the new member
+    const role = member.guild.roles.cache.get(config.roles.member);
+    if (role) {
+      member.roles.add(role).catch(console.error);
+    }
 
-		// Log the new member in the specified channel
-		const logChannel = member.guild.channels.cache.get(config.channels.log.id);
-		if (logChannel) {
-			logChannel.send(`[SYSTEM] **${ member.user.tag }** has joined the server.`);
-		} else {
-			console.log(`Log channel not found: ${ config.channels.log.id } 🔴`);
-		}
-	}
+    // Log the new member in the specified channel
+    const logChannel = member.guild.channels.cache.get(config.channels.log.id);
+    if (logChannel) {
+      logChannel.send(`[SYSTEM] **${member.user.tag}** has joined the server.`);
+    } else {
+      console.log(`Log channel not found: ${config.channels.log.id} 🔴`);
+    }
+  },
 };
